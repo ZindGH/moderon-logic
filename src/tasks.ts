cc7a4fa4a35d8d0cdb34b822503ac2be0002babd
YouTooLife <youtoolifesite@ya.ru>
2022-11-04 13:34:17 +0300

    Some Sketches

---
+++ src/tasks.ts
@@ -0,0 +1,139 @@
+import * as vscode from "vscode";
+import * as toolchain from "./toolchain";
+import { Config } from "./config";
+import { log } from "./util";
+
+
+export const TASK_TYPE = "eec";
+export const TASK_SOURCE = "eemblang";
+
+export interface EasyTaskDefinition extends vscode.TaskDefinition {
+    command?: string;
+    args?: string[];
+    cwd?: string;
+    env?: { [key: string]: string };
+    overrideCargo?: string;
+}
+
+class EasyTaskProvider implements vscode.TaskProvider {
+    private readonly config: Config;
+
+    constructor(config: Config) {
+        this.config = config;
+    }
+
+    async provideTasks(): Promise<vscode.Task[]> {
+        // Detect Rust tasks. Currently we do not do any actual detection
+        // of tasks (e.g. aliases in .cargo/config) and just return a fixed
+        // set of tasks that always exist. These tasks cannot be removed in
+        // tasks.json - only tweaked.
+
+        const defs = [
+            { command: "build", group: vscode.TaskGroup.Build },
+            { command: "check", group: vscode.TaskGroup.Build },
+            { command: "simulate", group: vscode.TaskGroup.Build },
+            { command: "clippy", group: vscode.TaskGroup.Build },
+            { command: "test", group: vscode.TaskGroup.Test },
+            { command: "clean", group: vscode.TaskGroup.Clean },
+            { command: "run", group: undefined },
+        ];
+
+        const tasks: vscode.Task[] = [];
+        for (const workspaceTarget of vscode.workspace.workspaceFolders || []) {
+            for (const def of defs) {
+                const vscodeTask = await buildEasyTask(
+                    workspaceTarget,
+                    { type: TASK_TYPE, command: def.command },
+                    `eec ${def.command}`,
+                    [def.command],
+                    this.config.easyRunner
+                );
+                vscodeTask.group = def.group;
+                tasks.push(vscodeTask);
+            }
+        }
+
+        return tasks;
+    }
+
+    async resolveTask(task: vscode.Task): Promise<vscode.Task | undefined> {
+        // VSCode calls this for every cargo task in the user's tasks.json,
+        // we need to inform VSCode how to execute that command by creating
+        // a ShellExecution for it.
+
+        const definition = task.definition as EasyTaskDefinition;
+
+        if (definition.type === TASK_TYPE && definition.command) {
+            const args = [definition.command].concat(definition.args ?? []);
+            //const args = ${"file"};
+            return await buildEasyTask(
+                task.scope,
+                definition,
+                task.name,
+                args,
+                this.config.easyRunner
+            );
+        }
+
+        return undefined;
+    }
+}
+
+export async function buildEasyTask(
+    scope: vscode.WorkspaceFolder | vscode.TaskScope | undefined,
+    definition: EasyTaskDefinition,
+    name: string,
+    args: string[],
+    customRunner?: string,
+    throwOnError: boolean = false
+): Promise<vscode.Task> {
+    let exec: vscode.ProcessExecution | vscode.ShellExecution | undefined = undefined;
+
+    if (customRunner) {
+        const runnerCommand = `${customRunner}.buildShellExecution`;
+        try {
+            const runnerArgs = { kind: TASK_TYPE, args, cwd: definition.cwd, env: definition.env };
+            const customExec = await vscode.commands.executeCommand(runnerCommand, runnerArgs);
+            if (customExec) {
+                if (customExec instanceof vscode.ShellExecution) {
+                    exec = customExec;
+                } else {
+                    log.debug("Invalid Easy ShellExecution", customExec);
+                    throw "Invalid Easy ShellExecution.";
+                }
+            }
+            // fallback to default processing
+        } catch (e) {
+            if (throwOnError) throw `Easy runner '${customRunner}' failed! ${e}`;
+            // fallback to default processing
+        }
+    }
+
+    if (!exec) {
+        // Check whether we must use a user-defined substitute for cargo.
+        // Split on spaces to allow overrides like "wrapper cargo".
+        const overrideEasy= definition.overrideEasy ?? definition.overrideEasy;
+        const easyPath = await toolchain.easyPath();
+        const easyCommand = overrideEasy?.split(" ") ?? [easyPath];
+
+        const fullCommand = [...easyCommand, ...args];
+
+        exec = new vscode.ProcessExecution(fullCommand[0], fullCommand.slice(1), definition);
+    }
+
+    return new vscode.Task(
+        definition,
+        // scope can sometimes be undefined. in these situations we default to the workspace taskscope as
+        // recommended by the official docs: https://code.visualstudio.com/api/extension-guides/task-provider#task-provider)
+        scope ?? vscode.TaskScope.Workspace,
+        name,
+        TASK_SOURCE,
+        exec,
+        ["$eec"]
+    );
+}
+
+export function activateTaskProvider(config: Config): vscode.Disposable {
+    const provider = new EasyTaskProvider(config);
+    return vscode.tasks.registerTaskProvider(TASK_TYPE, provider);
+}
---
+++ src/toolchain.ts
@@ -0,0 +1,205 @@
+import * as cp from "child_process";
+import * as os from "os";
+import * as path from "path";
+import * as readline from "readline";
+import * as vscode from "vscode";
+import { execute, log, memoizeAsync } from "./util";
+
+interface CompilationArtifact {
+    fileName: string;
+    name: string;
+    kind: string;
+    isTest: boolean;
+}
+
+export interface ArtifactSpec {
+    easyArgs: string[];
+    filter?: (artifacts: CompilationArtifact[]) => CompilationArtifact[];
+}
+
+export class Easy {
+    constructor(readonly rootFolder: string, readonly output: vscode.OutputChannel) {}
+
+    // Made public for testing purposes
+    static artifactSpec(args: readonly string[]): ArtifactSpec {
+        const easyArgs = [...args, "--message-format=json"];
+
+        // arguments for a runnable from the quick pick should be updated.
+        // see crates\rust-analyzer\src\main_loop\handlers.rs, handle_code_lens
+        switch (easyArgs[0]) {
+            case "run":
+                easyArgs[0] = "build";
+                break;
+            case "test": {
+                if (!easyArgs.includes("--no-run")) {
+                    easyArgs.push("--no-run");
+                }
+                break;
+            }
+        }
+
+        const result: ArtifactSpec = { easyArgs: easyArgs };
+        if (easyArgs[0] === "test" || easyArgs[0] === "bench") {
+            // for instance, `crates\rust-analyzer\tests\heavy_tests\main.rs` tests
+            // produce 2 artifacts: {"kind": "bin"} and {"kind": "test"}
+            result.filter = (artifacts) => artifacts.filter((it) => it.isTest);
+        }
+
+        return result;
+    }
+
+    private async getArtifacts(spec: ArtifactSpec): Promise<CompilationArtifact[]> {
+        const artifacts: CompilationArtifact[] = [];
+
+        try {
+            await this.runEasy(
+                spec.easyArgs,
+                (message) => {
+                    if (message.reason === "compiler-artifact" && message.executable) {
+                        const isBinary = message.target.crate_types.includes("bin");
+                        const isBuildScript = message.target.kind.includes("custom-build");
+                        if ((isBinary && !isBuildScript) || message.profile.test) {
+                            artifacts.push({
+                                fileName: message.executable,
+                                name: message.target.name,
+                                kind: message.target.kind[0],
+                                isTest: message.profile.test,
+                            });
+                        }
+                    } else if (message.reason === "compiler-message") {
+                        this.output.append(message.message.rendered);
+                    }
+                },
+                (stderr) => this.output.append(stderr)
+            );
+        } catch (err) {
+            this.output.show(true);
+            throw new Error(`Easy invocation has failed: ${err}`);
+        }
+
+        return spec.filter?.(artifacts) ?? artifacts;
+    }
+
+    async executableFromArgs(args: readonly string[]): Promise<string> {
+        const artifacts = await this.getArtifacts(Easy.artifactSpec(args));
+
+        if (artifacts.length === 0) {
+            throw new Error("No compilation artifacts");
+        } else if (artifacts.length > 1) {
+            throw new Error("Multiple compilation artifacts are not supported.");
+        }
+
+        return artifacts[0].fileName;
+    }
+
+    private async runEasy(
+        easyArgs: string[],
+        onStdoutJson: (obj: any) => void,
+        onStderrString: (data: string) => void
+    ): Promise<number> {
+        const path = await easyPath();
+        return await new Promise((resolve, reject) => {
+            const easy = cp.spawn(path, easyArgs, {
+                stdio: ["ignore", "pipe", "pipe"],
+                cwd: this.rootFolder,
+            });
+
+            easy.on("error", (err) => reject(new Error(`could not launch EEmbLang compiler: ${err}`)));
+
+            easy.stderr.on("data", (chunk) => onStderrString(chunk.toString()));
+
+            const rl = readline.createInterface({ input: easy.stdout });
+            rl.on("line", (line) => {
+                const message = JSON.parse(line);
+                onStdoutJson(message);
+            });
+
+            easy.on("exit", (exitCode, _) => {
+                if (exitCode === 0) resolve(exitCode);
+                else reject(new Error(`exit code: ${exitCode}.`));
+            });
+        });
+    }
+}
+
+/** Mirrors `project_model::sysroot::discover_sysroot_dir()` implementation*/
+export async function getSysroot(dir: string): Promise<string> {
+    const easyPath = await getPathForExecutable("eec");
+
+    // do not memoize the result because the toolchain may change between runs
+    return await execute(`${easyPath} --print sysroot`, { cwd: dir });
+}
+
+export async function getEasyId(dir: string): Promise<string> {
+    const easyPath = await getPathForExecutable("eec");
+
+    // do not memoize the result because the toolchain may change between runs
+    const data = await execute(`${easyPath} -V -v`, { cwd: dir });
+    const rx = /commit-hash:\s(.*)$/m;
+
+    return rx.exec(data)![1];
+}
+
+/** Mirrors `toolchain::cargo()` implementation */
+export function easyPath(): Promise<string> {
+    return getPathForExecutable("eec");
+}
+
+/** Mirrors `toolchain::get_path_for_executable()` implementation */
+export const getPathForExecutable = memoizeAsync(
+    // We apply caching to decrease file-system interactions
+    async (executableName: "eec" | "EEcompiler" | "easy" | "st-util"): Promise<string> => {
+        {
+            const envVar = process.env[executableName.toUpperCase()];
+            if (envVar) return envVar;
+        }
+
+        if (await lookupInPath(executableName)) return executableName;
+
+        try {
+            // hmm, `os.homedir()` seems to be infallible
+            // it is not mentioned in docs and cannot be inferred by the type signature...
+            const standardPath = vscode.Uri.joinPath(
+                vscode.Uri.file(os.homedir()),
+                ".eec",
+                "out", "build", "bin",
+                "Windows_NT" ? `${executableName}.exe` : executableName
+            );
+
+            if (await isFileAtUri(standardPath)) return standardPath.fsPath;
+        } catch (err) {
+            log.error("Failed to read the fs info", err);
+        }
+        return executableName;
+    }
+);
+
+async function lookupInPath(exec: string): Promise<boolean> {
+    const paths = process.env.PATH ?? "";
+
+    console.log(os.type());
+
+    const candidates = paths.split(path.delimiter).flatMap((dirInPath) => {
+        const candidate = path.join(dirInPath, exec);
+        return os.type() === "Windows_NT" ? [candidate, `${candidate}.exe`] : [candidate];
+    });
+
+    for await (const isFile of candidates.map(isFileAtPath)) {
+        if (isFile) {
+            return true;
+        }
+    }
+    return false;
+}
+
+async function isFileAtPath(path: string): Promise<boolean> {
+    return isFileAtUri(vscode.Uri.file(path));
+}
+
+async function isFileAtUri(uri: vscode.Uri): Promise<boolean> {
+    try {
+        return ((await vscode.workspace.fs.stat(uri)).type & vscode.FileType.File) !== 0;
+    } catch {
+        return false;
+    }
+}
